e17e66b6b536d4dec59be791703002aef0854bb3
Odst000 <100271951+Odst000@users.noreply.github.com>
2024-01-22 04:23:45 -0600

    Add files via upload

---
+++ JS/menu_contacto.js
@@ -0,0 +1,47 @@
+function boton1(){
+    document.getElementById('tarj1').style.display = 'block';
+    document.getElementById('tarj2').style.display = 'none';
+    document.getElementById('tarj3').style.display = 'none';
+
+
+    document.getElementById('butt1').style.background = "rgb(238, 87, 40)";
+    document.getElementById('butt1').style.color = "white";
+
+    document.getElementById('butt2').style.background = "lightsteelblue";
+    document.getElementById('butt2').style.color = "black";
+
+    document.getElementById('butt3').style.background = "lightsteelblue";
+    document.getElementById('butt3').style.color = "black";
+}
+
+function boton2(){
+    document.getElementById('tarj2').style.display = 'block';
+    document.getElementById('tarj1').style.display = 'none';
+    document.getElementById('tarj3').style.display = 'none';
+
+
+    document.getElementById('butt2').style.background = "rgb(238, 87, 40)";
+    document.getElementById('butt2').style.color = "white";
+
+    document.getElementById('butt1').style.background = "lightsteelblue";
+    document.getElementById('butt1').style.color = "black";
+
+    document.getElementById('butt3').style.background = "lightsteelblue";
+    document.getElementById('butt3').style.color = "black";
+}
+
+function boton3(){
+    document.getElementById('tarj3').style.display = 'block';
+    document.getElementById('tarj1').style.display = 'none';
+    document.getElementById('tarj2').style.display = 'none';    
+    
+    
+    document.getElementById('butt3').style.background = "rgb(238, 87, 40)";
+    document.getElementById('butt3').style.color = "white";
+
+    document.getElementById('butt2').style.background = "lightsteelblue";
+    document.getElementById('butt2').style.color = "black";
+
+    document.getElementById('butt1').style.background = "lightsteelblue";
+    document.getElementById('butt1').style.color = "black";
+}
